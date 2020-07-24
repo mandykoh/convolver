6989285fa3bd06b54c80e18e4ee68e4732460fa6
Amanda Koh <amanda@naucera.net>
2020-07-24 18:56:24 +1000

    Add test coverage over applying kernels.

--- kernel_test.go
+++ kernel_test.go
@@ -10,24 +10,13 @@ import (
 	"math/rand"
 	"os"
 	"runtime"
+	"sync"
 	"testing"
 	"time"
 )
 
 func BenchmarkParallelisation(b *testing.B) {
-
-	// Generate a large image with random pixels
-	inputImg := image.NewNRGBA(image.Rect(0, 0, 8192, 8192))
-	for i := inputImg.Rect.Min.Y; i < inputImg.Rect.Max.Y; i++ {
-		for j := inputImg.Rect.Min.X; j < inputImg.Rect.Max.X; j++ {
-			inputImg.SetNRGBA(j, i, color.NRGBA{
-				R: uint8(rand.Intn(256)),
-				G: uint8(rand.Intn(256)),
-				B: uint8(rand.Intn(256)),
-				A: uint8(rand.Intn(256)),
-			})
-		}
-	}
+	inputImg := randomImage(8192, 8192)
 
 	// Gaussian blur kernel
 	weights := []int32{
@@ -52,6 +41,49 @@ func BenchmarkParallelisation(b *testing.B) {
 				_ = kernel.ApplySum(inputImg, threadCount)
 			}
 		})
+	}
+}
+
+func TestApply(t *testing.T) {
+	img := randomImage(256, 256)
+
+	mutex := sync.Mutex{}
+	pixelsVisited := make(map[image.Point]struct{})
+
+	op := func(_ *image.NRGBA, x, y int) color.NRGBA {
+		mutex.Lock()
+		defer mutex.Unlock()
+
+		pixelsVisited[image.Pt(x, y)] = struct{}{}
+		return img.NRGBAAt(x, y)
+	}
+
+	kernel := KernelWithRadius(0)
+	result := kernel.apply(img, op, runtime.NumCPU())
+
+	if expected, actual := img.Rect, result.Rect; expected.Dx() != actual.Dx() || expected.Dy() != actual.Dy() {
+		t.Errorf("Expected resulting image to be %dx%d but was %dx%d", expected.Dx(), expected.Dy(), actual.Dx(), actual.Dy())
+	}
+
+	differentPixelCount := 0
+	pixelsNotVisited := 0
+
+	for i := img.Rect.Min.Y; i < img.Rect.Max.Y; i++ {
+		for j := img.Rect.Min.X; j < img.Rect.Max.X; j++ {
+			if expected, actual := img.NRGBAAt(j, i), result.NRGBAAt(j, i); expected != actual {
+				differentPixelCount++
+			}
+			if _, ok := pixelsVisited[image.Pt(j, i)]; !ok {
+				pixelsNotVisited++
+			}
+		}
+	}
+
+	if differentPixelCount > 0 {
+		t.Errorf("Expected resulting image and input image to match but they differ at %d pixels", differentPixelCount)
+	}
+	if pixelsNotVisited > 0 {
+		t.Errorf("Expected kernel operation to have been applied to all pixels but %d were not visited", pixelsNotVisited)
 	}
 }
 
@@ -256,4 +288,21 @@ func TestDilateErode(t *testing.T) {
 	if err != nil {
 		log.Fatalf("Error encoding output image: %v", err)
 	}
+}
+
+func randomImage(w, h int) *image.NRGBA {
+	img := image.NewNRGBA(image.Rect(0, 0, w, h))
+
+	for i := img.Rect.Min.Y; i < img.Rect.Max.Y; i++ {
+		for j := img.Rect.Min.X; j < img.Rect.Max.X; j++ {
+			img.SetNRGBA(j, i, color.NRGBA{
+				R: uint8(rand.Intn(256)),
+				G: uint8(rand.Intn(256)),
+				B: uint8(rand.Intn(256)),
+				A: uint8(rand.Intn(256)),
+			})
+		}
+	}
+
+	return img
 }
